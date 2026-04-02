6ea40b9fedc46500ac70d3e67434605293eeb0d4
Elin Iller <57793973+skatespresso@users.noreply.github.com>
2026-04-02 14:51:43 +0200

    Delete dndhub directory

--- dndhub/src/components/BusinessPlan.jsx
+++
@@ -1,6 +0,0 @@
-import React, { useState } from 'react'
-
-function BusinessPLan(){
-    return (<></>)
-}
-export default BusinessPlan
--- dndhub/src/components/Contact.jsx
+++
@@ -1,6 +0,0 @@
-import React, { useState } from 'react'
-
-function Contact(){
-    return (<></>)
-}
-export default Contact
